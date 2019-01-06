ce66d1c17190e8d251c5c7b96627306263d2b81f
Heather Zachernuk <35938539+heatherzachernuk@users.noreply.github.com>
2019-01-06 14:38:30 -0800

    Adding files

---
+++ script.js
@@ -0,0 +1,338 @@
+var index = {
+	当:{
+		pinyin: "dāng", 
+		hanzi:"当", 
+		definition: ["when"], 
+		image:"当.png"
+	},
+	我:{ 
+		pinyin: "wǒ", 
+		hanzi:"我", 
+		definition: ["I" , "me" , "myself"], 
+		image:"我.png"
+	},
+	还:{ 
+		pinyin: "hái", 
+		hanzi:"还", 
+		definition: ["also" , "yet"],
+		image:"还.png"
+	},
+	只有:{ 
+		pinyin: "zhǐyǒu", 
+		hanzi:"只有", 
+		definition: ["only"],
+		image:"只.png" 
+	},
+	六:{ 
+		pinyin: "liù", 
+		hanzi:"六", 
+		definition: ["six"],
+		image:"六.png" 
+	},
+	岁:{ 
+		pinyin: "suì", 
+		hanzi:"岁", 
+		definition: ["(measure word for years)"],
+		image:"岁.png" 
+	},
+	的:{ 
+		pinyin: "de", 
+		hanzi:"的", 
+		definition: ["(possessive particle)"],
+		image:"的.png" 
+	},
+	时候:{ 
+		pinyin: "shíhou", 
+		hanzi:"时候", 
+		definition: ["time"],
+		image:"时.png",
+		image2: "候.png"
+	},
+	在:{ 
+		pinyin: "zài", 
+		hanzi:"在", 
+		definition: ["in" , "be"],
+		image:"在.png" 
+	},
+	一:{ 
+		pinyin: "yī", 
+		hanzi:"一", 
+		definition: ["one"],
+		image:"一.png" 
+	},
+	本:{ 
+		pinyin: "běn", 
+		hanzi:"本", 
+		definition: ["this " , "source" , "origin"],
+		image:"本.png" 
+	},
+	描写:{ 
+		pinyin: "miáoxiě", 
+		hanzi:"描写", 
+		definition: ["description" , "depiction"],
+		image:"描.png",
+		image2: "写.png" 
+	},
+	原始:{ 
+		pinyin: "yuánshǐ", 
+		hanzi:"原始", 
+		definition: ["original" , "primitive"],
+		image:"原.png",
+		image2: "始.png"
+	},
+	森林:{ 
+		pinyin: "sēnlín", 
+		hanzi:"森林", 
+		definition: ["forest"],
+		image:"森.png",
+		image2: "林.png"
+	},
+	的:{ 
+		pinyin: "de", 
+		hanzi:"的", 
+		definition: ["(possessive particle)"],
+		image:"的.png" 
+	},
+	名:{ 
+		pinyin: "míng", 
+		hanzi:"名", 
+		definition: ["name"],
+		image:"名.png" 
+	},
+	叫:{ 
+		pinyin: "jiào", 
+		hanzi:"叫", 
+		definition: ["called"],
+		image:"叫.png" 
+	},
+	真实:{ 
+		pinyin: "zhēnshí", 
+		hanzi:"真实", 
+		definition: ["real" , "true"],
+		image:"真.png",
+		image2: "实.png" 
+	},
+	
+	的:{ 
+		pinyin: "de", 
+		hanzi:"的", 
+		definition: ["(possessive particle)"],
+		image:"的.png" 
+	},
+	故事:{ 
+		pinyin: "gùshì", 
+		hanzi:"故事", 
+		definition: ["story"],
+		image:"故.png",
+		image2: "事.png" 
+	},
+	
+	的:{ 
+		pinyin: "de", 
+		hanzi:"的", 
+		definition: ["(possessive particle)"],
+		image:"的.png" 
+	},
+	书:{ 
+		pinyin: "shū", 
+		hanzi:"书", 
+		definition: ["book", "letter", "write"],
+		image:"书.png" 
+	},
+	中:{ 
+		pinyin: "zhōng", 
+		hanzi:"中", 
+		definition: ["in", "during", "middle"],
+		image:"中.png" 
+	},
+	看:{ 
+		pinyin: "kàn", 
+		hanzi:"看", 
+		definition: ["look", "see", "read"],
+		image:"看.png" 
+	},
+	到了:{ 
+		pinyin: "dàole", 
+		hanzi:"到了", 
+		definition: ["to", "there were"],
+		image:"到.png",
+		image2: "了.png" 
+	},
+	
+	一:{ 
+		pinyin: "yī", 
+		hanzi:"一", 
+		definition: ["one"],
+		image:"一.png" 
+	},
+	副:{ 
+		pinyin: "fù", 
+		hanzi:"副", 
+		definition: ["deputy", "secondary"],
+		image:"副.png" 
+	},
+	精彩:{ 
+		pinyin: "jīngcǎi", 
+		hanzi:"精彩", 
+		definition: ["wonderful", "brilliant"],
+		image:"精.png",
+		image2: "彩.png" 
+	},
+	
+	的:{ 
+		pinyin: "de", 
+		hanzi:"的", 
+		definition: ["(possessive particle)"],
+		image:"的.png" 
+	},
+	插画:{ 
+		pinyin: "chāhuà", 
+		hanzi:"插画", 
+		definition: ["illustration"],
+		image:"插.png",
+		image2: "画.png" 
+	},
+	画:{ 
+		pinyin: "huà", 
+		hanzi:"画", 
+		definition: ["painting", "drawing", "draw"],
+		image:"画.png" 
+	},
+	
+	的:{ 
+		pinyin: "de", 
+		hanzi:"的", 
+		definition: ["(possessive particle)"],
+		image:"的.png" 
+	},
+	是:{ 
+		pinyin: "shì", 
+		hanzi:"是", 
+		definition: ["is", "be", "correct"],
+		image:"是.png" 
+	},
+	一条:{ 
+		pinyin: "yītiáo", 
+		hanzi:"一条", 
+		definition: ["one"],
+		image:"一.png", 
+		image2: "条.png"
+	},
+	蟒蛇:{ 
+		pinyin: "mǎngshé", 
+		hanzi:"蟒蛇", 
+		definition: ["python"],
+		image:"蟒.png",
+		image2: "蛇.png" 
+	},
+	正在:{ 
+		pinyin: "zhèngzài", 
+		hanzi:"正在", 
+		definition: ["are", "while"],
+		image:"正.png",
+		image2: "在.png" 
+	},
+	吞食:{ 
+		pinyin: "tūnshí", 
+		hanzi:"吞食", 
+		definition: ["devour", "swallow"],
+		image:"吞.png", 
+		image2: "食.png"
+	},
+	
+	一:{ 
+		pinyin: "yī", 
+		hanzi:"一", 
+		definition: ["one"],
+		image:"一.png" 
+	},
+	只:{ 
+		pinyin: "zhǐ", 
+		hanzi:"只", 
+		definition: ["only", "just", "but"],
+		image:"只.png" 
+	},
+	大:{ 
+		pinyin: "dà", 
+		hanzi:"大", 
+		definition: ["great", "big", "strong"],
+		image:"大.png" 
+	},
+	野兽:{ 
+		pinyin: "yěshòu", 
+		hanzi:"野兽", 
+		definition: ["wild beast"],
+		image:"野.png", 
+		image2: "兽.png"
+	},
+};
+
+var wordList = [];
+let currentHanziId = 0;
+let id = 0;
+
+var phrase = ["当", "我", "还", "只有", "六", "岁", "的", "时候", "，", "在", 
+	"一", "本", "描写", "原始", "森林", "的", "名", "叫", "《", "真实", "的", "故事", 
+	"》", "的", "书", "中", "，", "看", "到了", "一", "副", "精彩", "的", "插画", "，", 
+	"画", "的", "是", "一条", "蟒蛇", "正在", "吞食", "一", "只", "大", "野兽", "。"]
+
+for (var i = 0; i < phrase.length; i++) {
+	var word = index[phrase[i]];
+	var wordLink = document.createElement("a");
+		wordLink.innerText = phrase[i];
+		document.getElementById("hanzi-text").appendChild(wordLink);
+
+	if(word!=undefined){
+		wordLink.addEventListener("click", clickCharacter);
+		wordLink.id = id;
+		id++;
+		wordList.push(word.hanzi);
+		wordLink.classList.add("link");
+	}
+}
+
+//this gets the full index entry for each section
+function populateSlide(word){
+	var strokeBox1 = document.querySelector(".hanzi-image-1");
+	var strokeBox2 = document.querySelector(".hanzi-image-2");
+	document.getElementById("pinyin-word").innerText = word.pinyin;
+	document.getElementById("hanzi-word").innerText = word.hanzi;
+	document.getElementById("english-word").innerHTML = word.definition.join("<br/>");
+	strokeBox1.innerHTML = "<img src=hanzi/"+word.image+">";
+		if(word.image2!=null){
+			strokeBox2.innerHTML = "<img src=hanzi/"+word.image2+">";
+			strokeBox2.style.display = "";
+		}
+		else{
+			strokeBox2.style.display = "none";
+		}
+}
+
+function clickCharacter(event){
+	currentHanziId = event.target.id;
+	populateSlide(index[event.target.innerText])
+}
+
+document.getElementById("next").onclick = function() {
+	clickNext()
+};
+
+document.getElementById("prev").onclick = function() {
+	clickPrev()
+};
+
+function clickNext() {
+	if(currentHanziId >= 0 && currentHanziId < 40){
+		currentHanziId++;
+		let word = index[document.getElementById(currentHanziId).innerHTML];
+		populateSlide(word);
+	}
+}
+
+function clickPrev() {
+	if(currentHanziId > 0){
+		currentHanziId--;
+		let word = index[document.getElementById(currentHanziId).innerHTML];
+		populateSlide(word);
+	}
+}
